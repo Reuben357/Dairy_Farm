9f156acf097694f5825c9e4e9f68fef0775b4283
Gathii Reuben <gathiireuben@gmail.com>
2024-09-26 05:51:59 -0700

    Changed css href link path

--- templates/db/products_page.php
+++ templates/db/products_page.php
@@ -36,7 +36,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $product['name']; ?></title>
-    <link rel="stylesheet" href="/templates/css/products_page.css">
+    <link rel="stylesheet" href="../css/products_page.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
